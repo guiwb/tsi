f3eda9599a0bd78132ab707d8759214ef3da6f0d
gui <gui.wb@hotmail.com>
2025-03-24 21:21:16 -0300

    feat: init projects

---
+++ backend-one/php-docker/www/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if(true == false){
+            echo "true";
+        } else {
+            echo "false";
+        }   
+        echo "finalmente";
+    ?>
+</body>
+</html>
