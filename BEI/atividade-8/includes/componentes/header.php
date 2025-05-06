e849ffdfb3b5b25bfd668b6fb87c910e106aba93
Guilherme Barbosa <gui.wb@hotmail.com>
2025-05-05 22:30:41 -0300

    remove

--- BEI/atividade-8/includes/componentes/header.php
+++
@@ -1,3 +0,0 @@
-<header>
-    <h1> Título do Site </h1>
-</header>
--- BEI/atividade-8/includes/logica/conecta.php
+++
@@ -1,8 +0,0 @@
-<?php
-   try {
-    $conexao = new PDO("mysql:host=localhost; dbname=aula; charset=utf8", "root","");
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
-        }
-?>
