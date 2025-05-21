99779476ef768faf19b480783e593cd7ccbb8ab4
gui <gui.wb@hotmail.com>
2025-05-21 20:59:45 -0300

    feat: create navbar and logged in template

--- projeto-final/template/logged-in.template.php
+++ projeto-final/template/logged-in.template.php
@@ -9,18 +9,61 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Natare App - <?= $current_route['title'] ?></title>
 
-  <link rel="stylesheet" href="./style/reset.css">
+  <link rel="stylesheet" href="./assets/styles/reset.css">
+  <link rel="stylesheet" href="./assets/styles/ui/input.css">
+  <link rel="stylesheet" href="./assets/styles/ui/button.css">
+  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <style>
+    body {
+      width: 100%;
+      height: 100vh;
+      font-family: "Poppins", sans-serif;
+      background-color: #f9f9f9;
+    }
+
+    .container {
+      width: 90%;
+      margin: 0 auto;
+    }
+
+    @media (min-width: 640px) {
+      .container {
+        max-width: 640px;
+      }
+    }
+
+    @media (min-width: 768px) {
+      .container {
+        max-width: 768px;
+      }
+    }
+
+    @media (min-width: 1024px) {
+      .container {
+        max-width: 1024px;
+      }
+    }
+
+    @media (min-width: 1280px) {
+      .container {
+        max-width: 1280px;
+      }
+    }
+  </style>
 </head>
 
 <body>
-  <nav>Bem-vindo, <?= $_SESSION['user']['name'] ?></nav>
-  <form action="/logout" method="POST">
-    <button type="submit">Sair</button>
-  </form>
-
-  <?php
-  include 'view/' . $current_route['view'];
-  ?>
+  <section class="container">
+    <?php
+    include 'view/components/navbar.php';
+    include 'view/' . $current_route['view'];
+    ?>
+  </section>
 </body>
 
 </html>
---
+++ projeto-final/view/components/navbar.php
@@ -0,0 +1,110 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+?>
+
+<nav>
+    <div class="left">
+        <div class="logo"><a href="/">NAPP</a></div>
+
+        <ul>
+            <li <?php if ($uri == '/') echo 'class="active"' ?>><a href="/">Página inicial</a></li>
+            <li <?php if ($uri == '/equipes') echo 'class="active"' ?>><a href="/equipes">Equipes</a></li>
+            <li <?php if ($uri == '/treinos') echo 'class="active"' ?>><a href="/treinos">Treinos</a></li>
+            <li <?php if ($uri == '/usuarios') echo 'class="active"' ?>><a href="/usuarios">Usuários</a></li>
+            <li <?php if ($uri == '/ambiente') echo 'class="active"' ?>><a href="/ambiente">Ambiente</a></li>
+        </ul>
+    </div>
+
+    <div class="right">
+        <div class="profile">
+            <img src="assets/images/<?= $_SESSION['user']['profile_picture'] ?? 'profile_picture.png'; ?>" alt="User Profile Picture">
+        </div>
+
+        <div class="logout">
+            <form action="/logout" method="POST">
+                <button class="danger small rounded" type="submit">
+                    <span class="material-symbols-outlined">logout</span> Sair
+                </button>
+            </form>
+        </div>
+    </div>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 20px 0;
+        padding: 0 20px 0 40px;
+        border-radius: 60px;
+        background: linear-gradient(90deg, #7d6bf7 0%, #5038ED 100%);
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.05);
+        color: white;
+    }
+
+    .left {
+        display: flex;
+        align-items: center;
+    }
+
+    .left ul {
+        display: flex;
+        align-items: center;
+        list-style: none;
+        margin-left: 20px;
+        padding: 0;
+    }
+
+    .left li {
+        margin: 0 15px;
+        text-decoration: none;
+        font-size: 16px;
+        font-weight: 600;
+        transition: transform 0.2s ease-in-out;
+    }
+
+    .left li:hover {
+        transform: translateY(2px);
+    }
+
+    .left li.active {
+        background: white;
+        padding: 10px 20px;
+        border-radius: 30px;
+        color: #5038ED;
+        margin: 0;
+    }
+
+    .logo {
+        font-size: 14px;
+        font-weight: bold;
+    }
+
+    .right {
+        display: flex;
+        align-items: center;
+    }
+
+    .profile {
+        display: flex;
+        align-items: center;
+        margin-right: 20px;
+    }
+
+    .profile p {
+        margin-right: 10px;
+    }
+
+    .profile img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+
+    button span {
+        transform: scale(0.8);
+        margin-right: 5px;
+        margin-top: -2px;
+    }
+</style>
